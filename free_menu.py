a7119da807f91efe4bf10a4a78a47bf1564e2470
zhaojx <zhaojx@bitbays.com>
2018-09-13 16:42:33 +0800

    main

--- free_menu.py
+++ free_menu.py
@@ -1,10 +1,11 @@
 from app01.wx_robot import robot
 
-client = robot.client
-client.create_menu({
-    'button': [{
-        'type': 'click',
-        'name': '一个按钮',
-        'key': 'one'
-    }]
-})
+if __name__ == '__main__':
+    client = robot.client
+    client.create_menu({
+        'button': [{
+            'type': 'click',
+            'name': '一个按钮',
+            'key': 'one'
+        }]
+    })
